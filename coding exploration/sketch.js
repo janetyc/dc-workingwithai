a3193b5d23b061e56adf176b8cc2471e0c047270
janetyc <janetyc@gmail.com>
2023-05-08 15:22:46 +0200

    change line_width and stroke color

--- coding exploration/sketch.js
+++ coding exploration/sketch.js
@@ -7,19 +7,20 @@ function draw() {
   //Set the line gap size 
   let line_gap = 10; 
   //Set the line width 
-  let line_width = 2; 
+  let line_width = 3; 
   //Set the line color 
-  stroke(255,200, 0); 
+  
 
   //Loop through horizontal lines and draw vertical lines 
   for(let x = line_gap/2; x < width; x += line_gap){ 
     for(let y = line_gap/2; y < height; y += line_gap){ 
       //Generate a random bias factor to distort the line shape 
       let bias = random(-0.5, 0.5); 
-      
+  
+      stroke(random(255),200, 0);
       //Draw the lines 
       line(x + bias*line_gap, y - line_gap/2, x + bias*line_gap, y + line_gap/2); 
       strokeWeight(line_width); 
     } 
   } 
-}
+} 
