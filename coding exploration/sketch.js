862d56bf2e7e49b2982529efc81ba6b8db8088f8
janetyc <janetyc@gmail.com>
2023-05-08 15:51:20 +0200

    multi-layer pattern based on input data

--- coding exploration/sketch.js
+++ coding exploration/sketch.js
@@ -1,3 +1,5 @@
+let input_data = [5, 200, 30, 200, 40];
+
 function setup() { 
   createCanvas(windowWidth, windowHeight); 
   noLoop();
@@ -7,20 +9,21 @@ function draw() {
   //Set the line gap size 
   let line_gap = 10; 
   //Set the line width 
-  let line_width = 3; 
+  let line_width = 1.5; 
   //Set the line color 
   
-
-  //Loop through horizontal lines and draw vertical lines 
-  for(let x = line_gap/2; x < width; x += line_gap){ 
-    for(let y = line_gap/2; y < height; y += line_gap){ 
-      //Generate a random bias factor to distort the line shape 
-      let bias = random(-0.5, 0.5); 
-  
-      stroke(random(255),200, 0);
-      //Draw the lines 
-      line(x + bias*line_gap, y - line_gap/2, x + bias*line_gap, y + line_gap/2); 
-      strokeWeight(line_width); 
-    } 
+  for(let i = 0; i<input_data.length; i++){
+    stroke(random(255),random(200), random(100));
+    //Loop through horizontal lines and draw vertical lines 
+    for(let x = line_gap/2; x < width; x += line_gap){ 
+      for(let y = line_gap/2; y < height; y += line_gap){ 
+        //Generate a random bias factor to distort the line shape 
+        let bias = random(-0.5, 0.5)*input_data[i]; 
+        
+        //Draw the lines 
+        line(x + bias*line_gap, y - line_gap/2, x + bias*line_gap, y + line_gap/2); 
+        strokeWeight(line_width); 
+      } 
+    }  
   } 
 } 
