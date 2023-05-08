434b284fbdbeb23625c5734c263d7d061deab2f8
janetyc <janetyc@gmail.com>
2023-05-08 15:19:50 +0200

    fix issues

--- coding exploration/sketch.js
+++ coding exploration/sketch.js
@@ -1,5 +1,6 @@
 function setup() { 
   createCanvas(windowWidth, windowHeight); 
+  noLoop();
 } 
 
 function draw() { 
@@ -8,16 +9,17 @@ function draw() {
   //Set the line width 
   let line_width = 2; 
   //Set the line color 
-  stroke(255,200); 
+  stroke(255,200, 0); 
 
   //Loop through horizontal lines and draw vertical lines 
   for(let x = line_gap/2; x < width; x += line_gap){ 
     for(let y = line_gap/2; y < height; y += line_gap){ 
       //Generate a random bias factor to distort the line shape 
       let bias = random(-0.5, 0.5); 
+      
       //Draw the lines 
       line(x + bias*line_gap, y - line_gap/2, x + bias*line_gap, y + line_gap/2); 
       strokeWeight(line_width); 
     } 
   } 
-} 
+}
