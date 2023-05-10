89bbd22c108b1d8123a6ec0e4af0b4b7daa223a4
janetyc <janetyc@gmail.com>
2023-05-11 01:38:08 +0200

    change stroke color based on humidity value

--- coding exploration with data/sketch.js
+++ coding exploration with data/sketch.js
@@ -1,9 +1,8 @@
 let temperature_data;
-let i;
+let humidity_data;
 function setup() { 
   createCanvas(windowWidth, windowHeight); 
   background(255); 
-  stroke(0);
   strokeWeight(2)
   i=0;
   
@@ -11,12 +10,12 @@ function setup() {
   d3.json("forecast.json").then((data) => {
     console.log(data);
     temperature_data = data.hourly.temperature_2m;
-    print(temperature_data)
+    humidity_data = data.hourly.relativehumidity_2m;
     
-    //draw pattern
     for (let x = 0; x < windowWidth; x += 10) { 
       for (let y = 0; y < windowHeight; y += 10) { 
         let randomOffset = random(-5, 5);
+        stroke(0, humidity_data[i%humidity_data.length]*3, 0);
         line(x + randomOffset, y, x + randomOffset, y + temperature_data[i%temperature_data.length]);
         i++;
       }
