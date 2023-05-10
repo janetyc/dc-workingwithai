618a4a627a736d832ef82fa990c2bfd643c54359
janetyc <janetyc@gmail.com>
2023-05-11 00:35:44 +0200

    read weather data from forecast.json

--- coding exploration with data/sketch.js
+++ coding exploration with data/sketch.js
@@ -1,16 +1,27 @@
+let temperature_data;
+let i;
 function setup() { 
   createCanvas(windowWidth, windowHeight); 
   background(255); 
   stroke(0);
   strokeWeight(2)
+  i=0;
   
-  for (let x = 0; x < windowWidth; x += 10) { 
-    for (let y = 0; y < windowHeight; y += 10) { 
-      let randomOffset = random(-5, 5);
-      line(x + randomOffset, y, x + randomOffset, y + 10);
+  //read data from json
+  d3.json("forecast.json").then((data) => {
+    console.log(data);
+    temperature_data = data.hourly.temperature_2m;
+    print(temperature_data)
+    
+    //draw pattern
+    for (let x = 0; x < windowWidth; x += 10) { 
+      for (let y = 0; y < windowHeight; y += 10) { 
+        let randomOffset = random(-5, 5);
+        line(x + randomOffset, y, x + randomOffset, y + 10);
+      }
     }
-  }
-
+  });
+  
 } 
 
 function draw() {
